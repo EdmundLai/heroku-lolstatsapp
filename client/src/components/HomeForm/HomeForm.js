0d07581e937ab12fff92b99b74bae8b0f75286ee
Edmund Lai <edmund.lai.07@gmail.com>
2019-11-20 23:44:29 -0800

    Added redirect to stats page after submit

--- client/src/components/HomeForm/HomeForm.js
+++ client/src/components/HomeForm/HomeForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Redirect } from 'react-router-dom';
 import RequestHandler from '../RequestHandler/RequestHandler';
 import './HomeForm.css';
 
@@ -9,7 +9,9 @@ class HomeForm extends React.Component {
 
     this.state = {
       summName: "",
-      queueType: "normalBlind"
+      queueType: "normalBlind",
+      goToStats: false,
+      statsLoading: false
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -39,82 +41,101 @@ class HomeForm extends React.Component {
     })
     .catch(err => {
       console.log(err);
+    })
+    .finally(() => {
+      this.setState({
+        statsLoading: false,
+        goToStats: true
+      });
     });
 
-    
+    this.setState({
+      statsLoading: true
+    });
 
     event.preventDefault();
   }
 
   render() {
-    return (
-      <form className="HomeForm" onSubmit={this.handleSubmit}>
-        <div className="InputBoxes">
-          <label id="NameBox">
-            <div id="NameLabel">Summoner Name:</div>
-            <input
-            type="text"
-            name="summName"
-            value={this.state.summName}
-            onChange={this.handleChange}
-            required
-            />
-          </label>
-
-          <div className="queue-selector">
-            <div className="queue-selector-title">
-              Queue Type:
+    if(this.state.goToStats) {
+      return(
+        <Redirect to="/stats" />
+      );
+    } else if(this.state.statsLoading) {
+      return(
+        <p>Stats currently loading!</p>
+      );
+    } else {
+      return (
+        <form className="HomeForm" onSubmit={this.handleSubmit}>
+          <div className="InputBoxes">
+            <label id="NameBox">
+              <div id="NameLabel">Summoner Name:</div>
+              <input
+              type="text"
+              name="summName"
+              value={this.state.summName}
+              onChange={this.handleChange}
+              required
+              />
+            </label>
+  
+            <div className="queue-selector">
+              <div className="queue-selector-title">
+                Queue Type:
+              </div>
+              
+              <div className="queueOptions">
+                <label>
+                  <input
+                  type="radio"
+                  name="queueType"
+                  value="normalBlind"
+                  onChange={this.handleChange}
+                  defaultChecked
+                  />
+                  Normal Blind Pick
+                </label>
+  
+                <label>
+                  <input
+                  type="radio"
+                  name="queueType"
+                  value="normalDraft"
+                  onChange={this.handleChange}
+                  />
+                  Normal Draft Pick
+                </label>
+  
+                <label>
+                  <input
+                  type="radio"
+                  name="queueType"
+                  value="rankedSolo"
+                  onChange={this.handleChange}
+                  />
+                  Ranked Solo/Duo
+                </label>
+  
+                <label>
+                  <input
+                  type="radio"
+                  name="queueType"
+                  value="rankedFlex"
+                  onChange={this.handleChange}
+                  />
+                  Ranked Flex Pick
+                </label>
+              </div>
+              
             </div>
-            
-            <div className="queueOptions">
-              <label>
-                <input
-                type="radio"
-                name="queueType"
-                value="normalBlind"
-                onChange={this.handleChange}
-                defaultChecked
-                />
-                Normal Blind Pick
-              </label>
-
-              <label>
-                <input
-                type="radio"
-                name="queueType"
-                value="normalDraft"
-                onChange={this.handleChange}
-                />
-                Normal Draft Pick
-              </label>
-
-              <label>
-                <input
-                type="radio"
-                name="queueType"
-                value="rankedSolo"
-                onChange={this.handleChange}
-                />
-                Ranked Solo/Duo
-              </label>
-
-              <label>
-                <input
-                type="radio"
-                name="queueType"
-                value="rankedFlex"
-                onChange={this.handleChange}
-                />
-                Ranked Flex Pick
-              </label>
-            </div>
-            
           </div>
-        </div>
-
-        <input id="DataSubmitButton" type="submit" value="Submit" />
-      </form>
-    );
+  
+          <input id="DataSubmitButton" type="submit" value="Submit" />
+        </form>
+      );
+    }
+    
   }
 }
 
